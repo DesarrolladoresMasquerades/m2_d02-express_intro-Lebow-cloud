6fe13d38db2ed2fa4c16d8da77caa0d126ba1657
Daniel Milan <danielmilanvera@gmail.com>
2022-02-08 12:58:57 +0100

    Created First Express Server

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
+const { request, response } = require("express");
 const express = require("express");
+const res = require("express/lib/response");
 
 require("dotenv").config();
 
 const app = express();
 
-// Build the server here
+app.use(express.static("public"));
+
+
+app.all("/", (request, response)=>{
+    response.sendFile(__dirname + "/views/index.html");
+    
+})
+
+//THIS RESPONSE TO ALL HTTP VERBS
+app.all("/home", (req, res) => {
+  res.sendFile(__dirname + "/views/home.html");
+});
+
+app.listen(3000), ()=> {
+    console.log("App listening on port 3000")}
+
